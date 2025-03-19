bfbc55b49efdd61a46a642a0583675b389a14917
Adhan14 <adankendari2017@gmail.com>
2025-03-19 15:50:29 +0800

    update js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,10 @@
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
+    if (document.body.classList.contains("dark-mode")) {
+        document.body.style.backgroundColor = "#222";
+        document.body.style.color = "white";
+    } else {
+        document.body.style.backgroundColor = "#f4f4f4";
+        document.body.style.color = "#333";
+    }
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-    let style = document.createElement("style");
-    style.innerHTML = `
-        .dark-mode {
-            background-color: #222;
-            color: white;
-        }
-    `;
-    document.head.appendChild(style);
-});
